34ac2422808451c3b6d1e03ad1c3e00de849b6f0
k <ktf.ahmynket5317@gmail.com>
2024-07-02 21:17:34 +0000

    once finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,23 @@ class UsersController < ApplicationController
 
  def edit
   @user = User.find(params[:id])
- end
+  unless user.id == current_user.id
+    redirect_to post_images_path
+  end
  
+  @user = User.find(params[:id])
+end
+
  def update
     @user = User.find(params[:id])
+    unless user.id == current_user.id
+    redirect_to post_images_path
+    end
+ 
+    @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path (@user.id)
- end
+    redirect_to user_path(@user.id)
+end
  
  private
  def user_params
